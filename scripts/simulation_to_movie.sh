e76579d3f5f3e66b34291f950be57012d7a4af4f
Sascha Gerloff <sascha.gerloff@t-online.de>
2021-03-10 16:53:35 +0100

    switched to gif for animations

--- scripts/simulation_to_movie.sh
+++ scripts/simulation_to_movie.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <input directory> <output file mp4/gif>"
+    echo "Usage: $0 <input directory> <output file name>"
 else
-  ffmpeg -i $1/run_t_%d.png $2
+  ffmpeg -i $1/run_t_%d.png $2.mp4
+  ffmpeg -i $2.mp4 -vf "fps=10,scale=700:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $2.gif
+  rm $2.mp4
 fi
--- scripts/value_evolution_to_movie.sh
+++ scripts/value_evolution_to_movie.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <input directory> <output file mp4/gif> <max number of frames>"
+    echo "Usage: $0 <input directory> <output file name> <max number of frames>"
 else
   ffmpeg -i $1/value_map_t_%d.png -frames:v $3 $2
+  ffmpeg -i $2.mp4 -vf "fps=10,scale=700:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $2.gif
+  rm $2.mp4
 fi
